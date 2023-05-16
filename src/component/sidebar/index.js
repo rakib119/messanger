6a9175c44ea716fbcba098941037d3a5091cc954
rakib119 <rakibh01739@gmail.com>
2023-05-17 01:03:18 +0600

    Side bar

--- src/component/sidebar/index.js
+++ src/component/sidebar/index.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import Menu from './menu'
+import {GoSignOut} from 'react-icons/go'
+import {FiUploadCloud} from 'react-icons/fi'
 import "./style.css"
 const Sidebar = () => {
   return (
     < >
         <div className='sidebar'>
-            <div className='profile-picture'> 
-              <picture><img className='p-photo' src='./asset/images/profile/profile.jpeg' alt='profile'/></picture>
+          <div className='profile-info'>
+              <div className='profile-picture'> 
+                <picture><img className='p-photo' src='./asset/images/profile/profile.jpeg' alt='profile'/></picture>
+                <div className="profile-overlay">
+                  <div className='upload-icon'><FiUploadCloud/></div>
+                </div>
+                
+              </div>
+              <div className='user-des'>
+                <p>Rakib Hasan</p>
+              </div>
+          </div> 
+          <div className='menu-bar'>
+            <Menu/>
+          </div>
+          <div className='log-section'>
+            <div className='log-out'>
+              <GoSignOut/>
             </div>
-            <div className='menu_bar'>
-              <Menu/>
-            </div>
-            <div className='logout'>logout</div>
-
+          </div> 
         </div>
     </>
   )
--- src/component/sidebar/menu.js
+++ src/component/sidebar/menu.js
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import {AiOutlineHome} from 'react-icons/ai'
+import {BsFillChatDotsFill} from 'react-icons/bs'
+import {IoMdNotificationsOutline} from 'react-icons/io'
+import {AiOutlineSetting} from 'react-icons/ai'
+import {NavLink} from 'react-router-dom'
 const Menu = () => {
   return (
     <>
       <div className='menu-item'>
-
+        <NavLink className='menu-icon' to='/'>
+          <AiOutlineHome/>
+        </NavLink>
+        <div className='menu-icon'>
+          <BsFillChatDotsFill/>
+        </div> 
+        <div className='menu-icon'>
+          <IoMdNotificationsOutline/>
+        </div> 
+        <div className='menu-icon'> 
+          <AiOutlineSetting/>
+        </div> 
       </div>
     </>
   )

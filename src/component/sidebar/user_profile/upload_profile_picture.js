9ad8356510d1a65b6bf90770fdde61e336f7ab70
rakib119 <rakibh01739@gmail.com>
2023-05-20 23:27:43 +0600

    Upload pic modal design

--- src/component/sidebar/user_profile/upload_profile_picture.js
+++ src/component/sidebar/user_profile/upload_profile_picture.js
@@ -1,23 +1,25 @@
-import * as React from 'react';
+import React, { useRef } from 'react';
 import Box from '@mui/material/Box'; 
 import './upload_profile_picture.css';
 import Modal from '@mui/material/Modal';
-
+import {BsImages} from 'react-icons/bs'
  
 
 export default function UploadPhotoPopup({open,setOpen}) { 
   const handleClose = () => setOpen(false);
-
+  const photoInput = useRef(null);
+  console.log(photoInput);
   return (
     <div> 
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
+      <Modal open={open} onClose={handleClose} >
         <Box className="modal">
-           hhello
+          <input hidden type="file" ref={photoInput} accept='.jpg,.jpeg,.png'/>
+          <div className="upload-box">
+            <div className="upload-svg" onClick={()=>photoInput.current.click()}>
+                <BsImages/>
+                <span>Upload</span>
+            </div>
+          </div>
         </Box>
       </Modal>
     </div>

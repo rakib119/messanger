5c064dbcb87fa3f882bdd6e78efd5d5d5013b947
rakib119 <rakibh01739@gmail.com>
2023-05-30 23:02:22 +0600

    group list

---
+++ src/component/home/grouplist/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const grouplist = () => {
+  return (
+    <div className='group-list'>
+        group list
+    </div>
+  )
+}
+
+export default grouplist
--- src/pages/main_pages/home.js
+++ src/pages/main_pages/home.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react' 
 import './home.css'
+import Grid from '@mui/material/Grid';
+import GroupList from '../../component/home/grouplist';
 
 const Home = () => {
   return (
     // className='bg-green' style={{display:"flex", justifyContent:'center',gap:'30px',marginTop:'40px' }}
-    <div >
-       <p><Link to='/register'>  Sign Up </Link></p>
-       <p><Link to='/login'>  Sign In </Link></p>
+    <div > 
+      <Grid container spacing={2} className='home-page'>
+        <Grid item xs={4}>
+            <div className="comp-wrapper">
+              <GroupList/>
+            </div>
+            <div className="comp-wrapper">
+              Friend list
+            </div>
+        </Grid>
+        <Grid item xs={4}>
+            2
+        </Grid>
+        <Grid item xs={4}>
+            3
+        </Grid>
+      </Grid>
     </div>
     
   )
